99da3eb6172bbc7240af75c7b3676baec64387ea
Yuusuke Hasegawa <31399699+usk2000@users.noreply.github.com>
2021-07-27 11:58:40 +0900

    feat: UIテストを改善

--- ViewTestingSampleUITests/ViewTestingSampleUITests.swift
+++ ViewTestingSampleUITests/ViewTestingSampleUITests.swift
@@ -52,41 +52,17 @@ class ViewTestingSampleUITests: XCTestCase {
         //ログインフォームに移動
         app.cells.element(boundBy: 1).tap()
         
-        //メールアドレス欄を入力
+        //メールアドレス欄に入力
         app.textFields.firstMatch.tap()
         app.textFields.firstMatch.typeText("sample@example.com")
         
-        //先にパスワードを入力
+        //パスワードを入力
         app.secureTextFields.firstMatch.tap()
-        app.textFields.firstMatch.tap()
-        app.secureTextFields.firstMatch.tap()
-
-        //※キーボードが表示されない
+        app.secureTextFields.firstMatch.typeText("test1234")
+        app.keyboards.buttons["Return"].tap()
         
-        debugPrint(app.menuBars)
-        
-        XCTAssertTrue(app.keyboards.buttons["Return"].waitForExistence(timeout: 4.0))
-        app.keys["t"].tap()
-        app.keys["e"].tap()
-        app.keys["s"].tap()
-        app.keys["t"].tap()
-
-//        app.secureTextFields.firstMatch.typeText("test1234")
-        
-        debugPrint("*** keyboard buttons")
-        debugPrint(app.keyboards.buttons)
-        debugPrint(app.keyboards.menuButtons)
-        debugPrint(app.keyboards.menuItems)
-
-        
-//        app.keyboards.buttons["Return"].tap()
-//        app.secureTextFields.firstMatch.doubleTap()
-//        app.menuItems["Paste"].tap()
-        
-        //secureTextFieldのinputの仕方
-        //https://developer.apple.com/forums/thread/5910
-                
         sleep(2)
+        add(XCTAttachment(screenshot: app.screenshot()))
         
     }
 
